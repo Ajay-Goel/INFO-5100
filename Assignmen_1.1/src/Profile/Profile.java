c8428a6f13388af386d6b4ec83a66fcf708d3b3a
Ajay Goel <38635184+Ajay-Goel@users.noreply.github.com>
2018-04-22 17:47:18 -0400

    Add files via upload

---
+++ Assignmen_1.1/src/Profile/Profile.java
@@ -0,0 +1,180 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Profile;
+
+/**
+ *
+ * @author ajaygoel
+ */
+public class Profile {
+    
+    private String name;
+    private String geo_data;
+    private String dob;// doubt
+    private String tel_no;
+    private String fax_no;
+    private String email;
+    private String ssn;
+    private String medical_no;
+    private String benificiary_no;
+    private String account_no;
+    private String liscence_no;
+    private String vehical_uvi;
+    private String device_udi;
+    private String linked_in;
+    private String ip_address;//doubt
+    private String biometric_identifier;//doubt
+    private String profile_image;
+    private String unique_identification;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGeo_data() {
+        return geo_data;
+    }
+
+    public void setGeo_data(String geo_data) {
+        this.geo_data = geo_data;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getTel_no() {
+        return tel_no;
+    }
+
+    public void setTel_no(String tel_no) {
+        this.tel_no = tel_no;
+    }
+
+    public String getFax_no() {
+        return fax_no;
+    }
+
+    public void setFax_no(String fax_no) {
+        this.fax_no = fax_no;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getMedical_no() {
+        return medical_no;
+    }
+
+    public void setMedical_no(String medical_no) {
+        this.medical_no = medical_no;
+    }
+
+    public String getBenificiary_no() {
+        return benificiary_no;
+    }
+
+    public void setBenificiary_no(String benificiary_no) {
+        this.benificiary_no = benificiary_no;
+    }
+
+    public String getAccount_no() {
+        return account_no;
+    }
+
+    public void setAccount_no(String account_no) {
+        this.account_no = account_no;
+    }
+
+    public String getLiscence_no() {
+        return liscence_no;
+    }
+
+    public void setLiscence_no(String liscence_no) {
+        this.liscence_no = liscence_no;
+    }
+
+    public String getVehical_uvi() {
+        return vehical_uvi;
+    }
+
+    public void setVehical_uvi(String vehical_uvi) {
+        this.vehical_uvi = vehical_uvi;
+    }
+
+    public String getDevice_udi() {
+        return device_udi;
+    }
+
+    public void setDevice_udi(String device_udi) {
+        this.device_udi = device_udi;
+    }
+
+    public String getLinked_in() {
+        return linked_in;
+    }
+
+    public void setLinked_in(String linked_in) {
+        this.linked_in = linked_in;
+    }
+
+    public String getIp_address() {
+        return ip_address;
+    }
+
+    public void setIp_address(String ip_address) {
+        this.ip_address = ip_address;
+    }
+
+    public String getBiometric_identifier() {
+        return biometric_identifier;
+    }
+
+    public void setBiometric_identifier(String biometric_identifier) {
+        this.biometric_identifier = biometric_identifier;
+    }
+
+    public String getProfile_image() {
+        return profile_image;
+    }
+
+    public void setProfile_image(String profile_image) {
+        this.profile_image = profile_image;
+    }
+
+    public String getUnique_identification() {
+        return unique_identification;
+    }
+
+    public void setUnique_identification(String unique_identification) {
+        this.unique_identification = unique_identification;
+    }
+    
+    
+    
+    
+}
